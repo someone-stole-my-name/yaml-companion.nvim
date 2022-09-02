ee86a70730d598f6112905dd090b1d53d68756b5
Moshe Avni <mavni@netapp.com>
2022-09-03 02:25:32 +0300

    Revert requests.

--- lua/yaml-companion/lsp/requests.lua
+++ lua/yaml-companion/lsp/requests.lua
@@ -3,15 +3,6 @@ local M = {}
 local lsp = vim.lsp
 local sync_timeout = 1000
 
-local function get_yamlls_client(bufnr)
-  local clients = vim.tbl_filter(function(c)
-    return c.name == "yamlls"
-  end, lsp.buf_get_clients(bufnr))
-  if #clients > 0 then
-    return clients[1]
-  end
-end
-
 -- let the yamlls attached to {bufnr} know that we support
 -- schema selection and it should fire a 'yaml/schema/store/initialized'
 -- when ready to use it
@@ -21,7 +12,7 @@ M.support_schema_selection = function(bufnr, client)
   if bufnr == 0 then
     bufnr = vim.api.nvim_get_current_buf()
   end
-  client = client or get_yamlls_client(bufnr)
+  client = client or lsp.get_active_clients({ name = "yamlls", bufnr = bufnr })[1]
 
   if client then
     return client.notify("yaml/supportSchemaSelection")
@@ -35,7 +26,8 @@ M.get_all_jsonschemas = function(bufnr, client)
   if bufnr == 0 then
     bufnr = vim.api.nvim_get_current_buf()
   end
-  client = client or get_yamlls_client(bufnr)
+  client = client or lsp.get_active_clients({ name = "yamlls", bufnr = bufnr })[1]
+
   if client then
     return client.request_sync(
       "yaml/get/all/jsonSchemas",
@@ -53,7 +45,7 @@ M.get_jsonschema = function(bufnr, client)
   if bufnr == 0 then
     bufnr = vim.api.nvim_get_current_buf()
   end
-  client = client or get_yamlls_client(bufnr)
+  client = client or lsp.get_active_clients({ name = "yamlls", bufnr = bufnr })[1]
 
   if client then
     local schemas =

9b020b226c51a786a4cd82af8fcba9224b642ae7
Moshe Avni <mavni@netapp.com>
2022-09-17 16:16:35 +0300

    Fix UI no selection and no client (#17)
    * Fix UI no selection and no client

--- lua/yaml-companion/lsp/util.lua
+++ lua/yaml-companion/lsp/util.lua
@@ -7,8 +7,8 @@ local matchers = require("yaml-companion._matchers")._loaded
 --- @return table schemas: merged list of user-defined, server-provided, and matcher-provided yaml schemas
 M.get_all_yaml_schemas = function()
   local schemas = lsp.get_all_jsonschemas(0)
-
-  if schemas == nil then
+  if schemas == nil or vim.tbl_count(schemas.result or {}) == 0 then
+    vim.notify("Schemas not loaded yet.")
     return
   end
 
